cd62e11f0c3294ceb86d5a8f9803bd46ea63eab0
clasped003 <84282381+clasped003@users.noreply.github.com>
2021-10-11 18:32:50 -0700

    Name function update
    Added conditional for no-input entries. Has a bug where using getchar() while recursively looping the program creates issues after the first use of the warning entering input for user without a name. The following tries make it so that user has to press enter twice and also the function then removes the first letter from the name given after that. It is currently being worked around by just using return 1 instead of the error checking function which kicks the user back to the main menu. Am currently working on this, will try to use an error-detecting loop to first check and then move on to the function.

--- namefuncs.c
+++ namefuncs.c
@@ -1,15 +1,24 @@
-/*This Naming function takes in a string or numbers with or without spaces. You can set a max length and the strings are stored in variables.*/
+/*This Naming function takes in a string or numbers with or without spaces. You can set a max length and the strings are stored in variables.
 
-/* NOTES:
--change firstname var name to charname
--create namefuncs header file, plug into this file
--remove main function, replace name with function call to be called
-from the main game files
-
+NOTES:
+-clear getchar buffer to add repetitive use entries
+- - make it so user can press enter repeatedly and then enter a name
+when they want to and it will all still work.
+-add a function and variable to add a gender.
+- -maybe just use a char and a M, F, or O?
+-
 */
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include "intro.h"
+#include "menu.h"
+#include "quitfuncs.h"
+#include "subfuncs.h"
+#include "namefuncs.h"
 //sets max length for the name strings
-#define STRLEN 20
+#define STRLEN 25
 
 int readln(char s[], int maxlen){
 		char ch;
@@ -28,16 +37,36 @@ int readln(char s[], int maxlen){
 		}//while
 		s[i] = '\0';
 		return i;
-}
+}//readln
 
-int main(int argc, char **argv){
-		char firstname[STRLEN];
-		int len_firstname;
+int playerId(){
+		system("clear");
+		char pName[STRLEN];
+		int len_pName;
 		printf("Let the Journey begin.\n\n"); 
-		printf("What is your Name: ");
-		len_firstname = readln(firstname, STRLEN);
-		printf("Hello, %s\n", firstname);
-		printf("Length of firstname = %d", 
-		len_firstname);
-		return 0;
-}
+		printf("What is your Name (25 Characters max): ");
+		
+		//Fix this by clearing buffer for getchar and then test, maybe do while loop
+		getchar();
+		len_pName = readln(pName, STRLEN);		
+		if(len_pName == 0 || pName[0] ==' ' || pName[0] == '\n'){
+		  puts("You must enter a name, try again.");			
+			sleep(1);							
+			return playerId();
+		}	else{printf("\nHello, %s\n", pName);
+		/*printf("Length of Name = %d \n", 
+		len_pName);*/			
+		puts("Please press 'Enter' to return to main menu.");
+		int a;
+		a = getchar();
+		if(a == '\n'){
+			return 1;	
+		}else{
+			 system("clear");	
+			 printf("ERROR: You entered %c \n", a);	
+			 puts("Incorrect entry. \n");
+			 puts("Please press 'Enter' to return to main menu.");			
+			 return 1;		 
+		}// if else}
+}		
+}// playerId
---
+++ namefuncs.h
@@ -0,0 +1,6 @@
+#ifndef NAMEFUNCS_H
+#define NAMEFUNCS_H
+
+int playerId();
+
+#endif
